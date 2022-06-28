c10c1c167b01fc0677612472e3fd880bec792f37
Dispersioned <dispersioned@gmail.com>
2022-06-28 15:25:47 +0300

    fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
+  typography: {
+    fontFamily: 'Poppins, sans-serif',
+  },
   palette: {
     primary: {
       // light: '#1C7D72',
