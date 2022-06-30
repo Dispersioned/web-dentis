7f0af5e2f1eb8cda2189931a79a93fb13e425136
Dispersioned <dispersioned@gmail.com>
2022-06-30 22:55:14 +0300

    feat: basic page layout

--- src/pages/info/index.jsx
+++ src/pages/info/index.jsx
@@ -1,14 +1,36 @@
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import { Link } from 'gatsby';
 import React from 'react';
 
 import Layout from '../../components/Layout';
 
-const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-);
+const InfoIndexPage = () => {
+  const blocks = [
+    { text: 'Органы контроля', link: './inspection' },
+    { text: 'Условия оплаты', link: './payment-conditions' },
+    { text: 'Правила оказания услуг', link: './code-of-service' },
+    { text: 'Бесплатная помощь', link: './free-help' },
+    { text: 'Гарантии', link: './guaranty' },
+    { text: 'Внутренний порядок', link: './in-order' },
+    { text: 'Полис ДМС', link: './dms' },
+    { text: 'Лицензии', link: './licenses' },
+    { text: 'Вакансии', link: './career' },
+  ];
 
-export default NotFoundPage;
+  return (
+    <Layout>
+      <Typography variant="h1">Клиентам</Typography>
+      <Grid container spacing={3}>
+        {blocks.map((block) => (
+          <Grid item xs={4}>
+            <Button component={Link} to={block.link}>
+              <Typography>{block.text}</Typography>
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
+  );
+};
+
+export default InfoIndexPage;
