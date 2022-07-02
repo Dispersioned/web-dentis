ac1323fa601ba0a44577feb0ac7b6207a46ffd69
Dispersioned <dispersioned@gmail.com>
2022-07-02 23:10:01 +0300

    feat: page subtitle bold weight

--- src/components/info/subtitle/Subtitle.jsx
+++ src/components/info/subtitle/Subtitle.jsx
@@ -2,7 +2,11 @@ import { Typography } from '@mui/material';
 import React from 'react';
 
 const Subtitle = ({ text }) => {
-  return <Typography variant="h5">{text}</Typography>;
+  return (
+    <Typography variant="h5" fontWeight={600}>
+      {text}
+    </Typography>
+  );
 };
 
 export default Subtitle;
