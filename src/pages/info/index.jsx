869d54b5241a80a1bae5fb06045b34d9bdc6656c
Dispersioned <dispersioned@gmail.com>
2022-07-02 12:41:12 +0300

    fix: prevent styles file from being a page

--- src/pages/info/index.jsx
+++ src/pages/info/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import Layout from '../../components/Layout';
 import PageTitle from '../../components/ui/page-title/PageTitle';
-import { Block, Content } from './style';
+import { Block, Content } from '../../styles/info/style.js';
 
 const InfoIndexPage = () => {
   const blocks = [
