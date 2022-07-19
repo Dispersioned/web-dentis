2e26193fd81e4e12905f23fa5ea51723e1b2b488
Dispersioned <dispersioned@gmail.com>
2022-07-19 13:29:34 +0300

    ref: paragraph component reexport

--- src/components/info/parapgraph/Paragraph.jsx
+++ src/components/info/parapgraph/Paragraph.jsx
@@ -5,8 +5,6 @@ const Body = styled('div')`
   margin-top: 1.2rem;
 `;
 
-const Paragraph = ({ children }) => {
+export const Paragraph = ({ children }) => {
   return <Body>{children}</Body>;
 };
-
-export default Paragraph;
--- src/templates/payment-conditions-page/payment-conditions-page.jsx
+++ src/templates/payment-conditions-page/payment-conditions-page.jsx
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 import React from 'react';
 
 import Layout from '../../components/Layout';
-import Paragraph from '../../components/info/parapgraph/Paragraph';
+import { Paragraph } from '../../components/info/parapgraph';
 import Subtitle from '../../components/info/subtitle/Subtitle';
 import PageTitle from '../../components/ui/page-title/PageTitle';
 
