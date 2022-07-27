6af8207569d1c45ae110f29738d6b78b219c8923
Dispersioned <dispersioned@gmail.com>
2022-07-27 18:51:46 +0300

    feat: create content layout

--- src/templates/dms-page/dms-page.jsx
+++ src/templates/dms-page/dms-page.jsx
@@ -1,9 +1,8 @@
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import { graphql } from 'gatsby';
 import React from 'react';
 
 import { Layout } from '../../components/Layout';
-import romanize from '../../services/romanize';
 import { BasePageTitle } from '../../shared/ui/base-page-title';
 import { BaseParagraph } from '../../shared/ui/base-paragraph';
 import { BaseSubtitle } from '../../shared/ui/base-subtitle';
@@ -20,7 +19,21 @@ export const DmsPageTemplate = ({ props }) => {
           <Typography>{data.text}</Typography>
         </BaseParagraph>
       ))}
-      text
+      <BaseSubtitle text="Наши партнеры:" />
+      {partners.map((partner, index) => (
+        <BaseParagraph key={index}>
+          <Typography>
+            {partner.type}{' '}
+            {partner.link ? (
+              <Link href={partner.link} target="_blank" fontWeight="bold" color="inherit" underline="hover">
+                {partner.title}
+              </Link>
+            ) : (
+              <Typography>{partner.title}</Typography>
+            )}
+          </Typography>
+        </BaseParagraph>
+      ))}
     </Layout>
   );
 };
